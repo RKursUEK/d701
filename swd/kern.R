1d39f78f3bfcb58e5b95ec292980c995918124a6
RKursUEK <kikso.kursr@onet.pl>
2025-02-27 09:56:04 +0100

    Update kern.R

--- swd/kern.R
+++ swd/kern.R
@@ -1,55 +1,55 @@
-x<-c(rnorm(50,10,2),rnorm(50,15,5))
-#Histogram z pakietu stats
-hist(x, breaks="Sturges", freq=FALSE, density=4,col="red") 
-# domyœlnie liczba klas histogramu wyznaczana wg regu³y Sturgesa 
-# (breaks="Sturges"): k = 1 + log2 n, inne mo¿liwoœci: regu³a Scotta 
-# (breaks="Scott") albo regu³a Freedmana-Diaconisa (breaks="FD")
-hist(x, breaks="Scott", freq=FALSE, density=4,col="green") 
-hist(x, breaks="FD", freq=FALSE, density=4,col="blue")
+x <- c(rnorm(50, 10, 2), rnorm(50, 15, 5))
+# Histogram z pakietu stats
+hist(x, breaks = "Sturges", freq=FALSE, density=4,col="red") 
+# domyslnie liczba klas histogramu wyznaczana wedlug reguly Sturgesa 
+# (breaks = "Sturges"): k = 1 + log2 n, inne mozliwoÅ“ci: regula Scotta 
+# (breaks = "Scott") albo regula Freedmana-Diaconisa (breaks = "FD")
+hist(x, breaks = "Scott", freq = FALSE, density = 4, col = "green") 
+hist(x, breaks = "FD", freq = FALSE, density = 4, col = "blue")
 
-#Estmator j¹drowy gêstoœci z pakietu stats
-density(x,bw="nrd0",kernel=gaussian")
+# Estymator jadrowy gestosci z pakietu stats
+density(x, bw = "nrd0", kernel = "gaussian")
 
 # http://cran.r-project.org/web/packages/mosaic/vignettes/GraphicsWithMosaic.html
 # histogram
 # densityplot
 
-#Estymatory j¹drowe funkcji gêstoœci
+# Estymatory jadrowe funkcji gestosci
 library(KernSmooth) 
 
-(h.opt<-dpik(x, scalest = "minim", level = 2L, kernel = "normal",   
+(h.opt <- dpik(x, scalest = "minim", level = 2L, kernel = "normal",   
      canonical = FALSE, gridsize = 401L, range.x = range(x), 
      truncate = TRUE))
 
-kern<-bkde(x,kernel="normal",bandwidth=h.opt)
+kern <- bkde(x, kernel = "normal", bandwidth = h.opt)
 names(kern)
-plot(bkde(x,kernel="normal",bandwidth=h.opt))
+plot(bkde(x, kernel = "normal", bandwidth = h.opt))
 
 example(bkde2D)
 
-dpih(x) #Optymalna szerokoœæ przedzia³u klasowego wg regu³y Scotta
+dpih(x) # Optymalna szerokosc przedzialu klasowego wedlug reguly Scotta
 
-# Inne pakiety z funkcjami zwi¹zanymi z estymatorami j¹drowymi 
+# Inne pakiety z funkcjami zwiazanymi z estymatorami jadrowymi 
 # package kernlab
 # package np
 # package sm
 
-# Pakiety umo¿liwiaj¹ce tworzenie histogramów 
-# package library(HistogramTools) # np. miary dywergencji histogramów - dywergencja Kullbacka-Leiblera
+# Pakiety umozliwiajace tworzenie histogramow 
+# package library(HistogramTools) # np. miary dywergencji histogramÃ³w - dywergencja Kullbacka-Leiblera
 # package histogram
 
 ## package MASS
 library(MASS)
-hist.FD(x) # Freedman–Diaconis
-hist.scott(x, prob = TRUE) #Scott 
+hist.FD(x) # Freedmanâ€“Diaconis
+hist.scott(x, prob = TRUE) # Scott 
 
 
 library(aplpack) 
 # m.in. histogramy z suwakiem dla liczby klas,
-# wykresy j¹drowych estymatorów gêstoœci z suwakiem 
-# dla parametru wyg³adzania i typu j¹dra
+# wykresy jadrowych estymatorow gestosci z suwakiem 
+# dla parametru wygladzania i typu jadra
 
-slider.hist(x,col="red")
+slider.hist(x, col = "red")
 slider.density(x)
 
 
